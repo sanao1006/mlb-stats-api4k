214a2655ea3a5a495e2064013342d598ae668adb
sanao1006 <naosak1006@gmail.com>
2023-08-30 21:52:28 +0900

    Add serialization class for `/awards` endpoint

---
+++ src/main/kotlin/response/Award.kt
@@ -0,0 +1,19 @@
+package response
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Award(
+    @Json(name = "description")
+    val description: String,
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "name")
+    val name: String,
+    @Json(name = "sortOrder")
+    val sortOrder: Int,
+    @Json(name = "sport")
+    val sport: Sport
+)
