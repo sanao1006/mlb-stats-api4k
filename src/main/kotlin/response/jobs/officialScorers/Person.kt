e855d54a9b9ea86095eb98ca75a322d111a26f2c
sanao1006 <naosak1006@gmail.com>
2023-08-31 22:08:52 +0900

    add: Serialization class of `/jobs/officialScorers` response

---
+++ src/main/kotlin/response/jobs/officialScorers/Person.kt
@@ -0,0 +1,15 @@
+package response.jobs.officialScorers
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Person(
+    @Json(name = "fullName")
+    val fullName: String,
+    @Json(name = "id")
+    val id: Int,
+    @Json(name = "link")
+    val link: String
+)
