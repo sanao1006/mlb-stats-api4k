cb6f8f19d46ccaf45e72903cf7a79307791b908b
sanao1006 <naosak1006@gmail.com>
2023-08-31 13:08:31 +0900

    feat: add `getConferences()` to MlbStatsApiImpl (WIP)

--- src/main/kotlin/MlbStatsApiImpl.kt
+++ src/main/kotlin/MlbStatsApiImpl.kt
@@ -6,14 +6,25 @@ import org.http4k.core.Method
 import org.http4k.core.Request
 import response.Award
 import response.AwardsResponse
+import response.Conference
 
 class MlbStatsApiImpl(val apiHost: String) : MlbStatsApi {
     val client: HttpHandler = JavaHttpClient()
     private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
+    /**
+     * Endpoint `/awards`
+     */
     override fun getAwards(): List<Award> {
         val request = client(Request(Method.GET, "$apiHost/awards")).body.toString()
         val awardsAdapter =  moshi.adapter<AwardsResponse>(AwardsResponse::class.java)
         return awardsAdapter.fromJson(request)!!.awards
+    }
+
+    /**
+     * Endpoint `conferences`
+     */
+    override fun getConferences(): List<Conference> {
+        TODO("Not yet implemented")
     }
 }
