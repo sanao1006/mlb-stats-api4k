ec9dc932df4001ed837fdb19a6e036f38d1a3016
sanao1006 <naosak1006@gmail.com>
2023-08-31 13:48:00 +0900

    add: add `getSports()` to MlbStatsApi

--- src/main/kotlin/MlbStatsApi.kt
+++ src/main/kotlin/MlbStatsApi.kt
@@ -1,9 +1,11 @@
 import response.Award
 import response.Conference
 import response.Division
+import response.Sports
 
 interface MlbStatsApi {
     fun getAwards(): List<Award>
     fun getConferences(): List<Conference>
     fun getDivisions(): List<Division>
+    fun getSports(): List<Sports>
 }
