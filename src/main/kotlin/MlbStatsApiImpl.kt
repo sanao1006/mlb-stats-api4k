83bfe9a343d93a7ee0f840e946ef3dd274cbe6e1
sanao1006 <naosak1006@gmail.com>
2023-08-31 13:39:21 +0900

    feat: Implement `getDivisions()` to receive `/divisions` responses

--- src/main/kotlin/MlbStatsApiImpl.kt
+++ src/main/kotlin/MlbStatsApiImpl.kt
@@ -8,7 +8,8 @@ import response.Award
 import response.AwardsResponse
 import response.Conference
 import response.ConferencesResponse
-
+import response.Division
+import response.DivisionsResponse
 class MlbStatsApiImpl(val apiHost: String) : MlbStatsApi {
     val client: HttpHandler = JavaHttpClient()
     private val moshi: Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
@@ -23,11 +24,20 @@ class MlbStatsApiImpl(val apiHost: String) : MlbStatsApi {
     }
 
     /**
-     * Endpoint `conferences`
+     * Endpoint `/conferences`
      */
     override fun getConferences(): List<Conference> {
         val responseBody = client(Request(Method.GET, "$apiHost/conferences")).body.toString()
         val conferencesAdapter = moshi.adapter<ConferencesResponse>(ConferencesResponse::class.java)
         return conferencesAdapter.fromJson(responseBody)?.conferences.orEmpty()
+    }
+
+    /**
+     * Endpoint `/divisions`
+     */
+    override fun getDivisions(): List<Division> {
+        val responseBody = client(Request(Method.GET, "$apiHost/divisions")).body.toString()
+        val divisionsAdapter = moshi.adapter<DivisionsResponse>(DivisionsResponse::class.java)
+        return divisionsAdapter.fromJson(responseBody)?.divisions.orEmpty()
     }
 }
