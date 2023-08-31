aa8db317fe4cae9c8799e21b8b34923073c7b7e8
sanao1006 <naosak1006@gmail.com>
2023-08-31 22:04:16 +0900

    fix: add comment to each endpoint

--- src/main/kotlin/MlbStatsApi.kt
+++ src/main/kotlin/MlbStatsApi.kt
@@ -5,13 +5,48 @@ import response.schedule.postseason.SchedulePostseasonResponse
 import response.schedule.postseason.series.SchedulePostseasonSeriesResponse
 
 interface MlbStatsApi {
+    /**
+     * Endpoint `/awards`
+     */
     fun getAwards(): List<Award>
+
+    /**
+     * Endpoint `/conferences`
+     */
     fun getConferences(): List<Conference>
+
+    /**
+     * Endpoint `/divisions`
+     */
     fun getDivisions(): List<Division>
+
+    /**
+     * Endpoint `/sports`
+     */
     fun getSports(): List<Sports>
+
+    /**
+     * Endpoint `/teams`
+     */
     fun getTeams(): List<Team>
+
+    /**
+     * Endpoint `/schedule/postseason/series`
+     */
     fun getSchedulePostseasonSeries(): SchedulePostseasonSeriesResponse
+
+    /**
+     * Endpoint `/schedule/postseason`
+     */
     fun getSchedulePostseason(): SchedulePostseasonResponse
+
+    /**
+     * Endpoint `/jobs/umpires`
+     */
     fun getJobsUmpires(): JobsUmpiresResponse
+
+    /**
+     * Endpoint `/jobs/datacasters`
+     */
     fun getJobsDatacasters(): JobsDatacastersResponse
 }
