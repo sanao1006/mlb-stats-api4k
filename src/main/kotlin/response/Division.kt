410199027fc288576f8d58183bf8e60bcb28421c
sanao1006 <naosak1006@gmail.com>
2023-08-31 14:16:05 +0900

    fix: made `Division`'s property nullable

--- src/main/kotlin/response/Division.kt
+++ src/main/kotlin/response/Division.kt
@@ -7,27 +7,27 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Division(
     @Json(name = "abbreviation")
-    val abbreviation: String,
+    val abbreviation: String?,
     @Json(name = "active")
-    val active: Boolean,
+    val active: Boolean?,
     @Json(name = "hasWildcard")
-    val hasWildcard: Boolean,
+    val hasWildcard: Boolean?,
     @Json(name = "id")
     val id: Int,
     @Json(name = "league")
-    val league: League,
+    val league: League?,
     @Json(name = "link")
     val link: String,
     @Json(name = "name")
     val name: String,
     @Json(name = "nameShort")
-    val nameShort: String,
+    val nameShort: String?,
     @Json(name = "numPlayoffTeams")
     val numPlayoffTeams: Int?,
     @Json(name = "season")
-    val season: String,
+    val season: String?,
     @Json(name = "sortOrder")
-    val sortOrder: Int,
+    val sortOrder: Int?,
     @Json(name = "sport")
-    val sport: Sport
+    val sport: Sport?
 )
