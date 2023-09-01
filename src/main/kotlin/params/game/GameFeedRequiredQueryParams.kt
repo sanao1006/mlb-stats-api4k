19b34d0eeb017c3fd1778d0a8fc2c0fd0e5529f5
sanao1006 <naosak1006@gmail.com>
2023-09-01 16:26:27 +0900

    fix: remove `plus()` in `GameFeedRequiredQueryParams` (It's unnecessary.)

--- src/main/kotlin/params/game/GameFeedRequiredQueryParams.kt
+++ src/main/kotlin/params/game/GameFeedRequiredQueryParams.kt
@@ -1,8 +1,4 @@
 package params.game
 
 
-data class GameFeedRequiredQueryParams(val gamePk: String){
-    fun plus(gameFeedOptionalQueryParams: GameFeedOptionalQueryParams): Map<String, String?> {
-        return mapOf("gamePk" to gamePk).plus(gameFeedOptionalQueryParams.toMap().filterValues { it -> it != null })
-    }
-}
+data class GameFeedRequiredQueryParams(val gamePk: String)
