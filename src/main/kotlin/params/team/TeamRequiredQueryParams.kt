96e8488496cce80c0808ded76168629bff10375d
sanao1006 <naosak1006@gmail.com>
2023-09-03 09:44:19 +0900

    update: update data class `TeamRequiredQueryParams`

--- src/main/kotlin/params/team/TeamRequiredQueryParams.kt
+++ src/main/kotlin/params/team/TeamRequiredQueryParams.kt
@@ -1,3 +1,7 @@
 package params.team
 
-data class TeamRequiredQueryParams(val teamId: String)
+data class TeamRequiredQueryParams(val teamId: String) {
+    fun plus(teamOptionalQueryParams: TeamOptionalQueryParams): Map<String, String?> {
+        return mapOf("teamId" to teamId).plus(teamOptionalQueryParams.toMap())
+    }
+}
