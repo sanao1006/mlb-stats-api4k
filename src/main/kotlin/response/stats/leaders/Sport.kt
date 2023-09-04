892ceb5cd5305a5b96a7feb9be21b12b8ee89c30
sanao1006 <naosak1006@gmail.com>
2023-09-04 11:02:04 +0900

    add: Serialization class of `stats_leaders` response

---
+++ src/main/kotlin/response/stats/leaders/Sport.kt
@@ -0,0 +1,15 @@
+package response.stats.leaders
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Sport(
+    @Json(name = "abbreviation")
+    val abbreviation: String, // MLB
+    @Json(name = "id")
+    val id: Int, // 1
+    @Json(name = "link")
+    val link: String // /api/v1/sports/1
+)
