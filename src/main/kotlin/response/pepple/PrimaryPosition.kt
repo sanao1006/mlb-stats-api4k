6388f697e3a6bf65f3e3f713ddc53185b4aa7263
sanao1006 <naosak1006@gmail.com>
2023-09-07 22:07:54 +0900

    add: Serialization class of `people` response

---
+++ src/main/kotlin/response/pepple/PrimaryPosition.kt
@@ -0,0 +1,17 @@
+package response.pepple
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PrimaryPosition(
+    @Json(name = "abbreviation")
+    val abbreviation: String, // TWP
+    @Json(name = "code")
+    val code: String, // Y
+    @Json(name = "name")
+    val name: String, // Two-Way Player
+    @Json(name = "type")
+    val type: String // Two-Way Player
+)
